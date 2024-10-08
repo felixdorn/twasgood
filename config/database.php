176ff6769591a3642d7e1d52a88bcd664ae0b48a
Félix Dorn <git@felixdorn.fr>
2024-10-08 12:17:22 +0200

    connect to postgres using a socket

--- config/database.php
+++ config/database.php
@@ -65,9 +65,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
+            'unix_socket' => env('DB_SOCKET'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
